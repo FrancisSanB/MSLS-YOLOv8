6f0107be29991ea68a32c98b38cc728a22a8bf5d
Francis Fernandez <fosb.promo@gmail.com>
2024-02-01 08:59:01 -0800

    final commit before defense

--- YOLOv8/.ipynb_checkpoints/enlarge_images-checkpoint.py
+++ YOLOv8/.ipynb_checkpoints/enlarge_images-checkpoint.py
@@ -3,16 +3,16 @@
 from PIL import Image
 import os
 
-def resize_images(input_folder, output_folder, target_size=(640, 640)):
-    # Ensure the output folder exists
-    os.makedirs(output_folder, exist_ok=True)
+def resize_images(input_output_folder, target_size=(640, 640)):
+    # Ensure the folder exists
+    os.makedirs(input_output_folder, exist_ok=True)
 
-    # List all files in the input folder
-    input_files = os.listdir(input_folder)
+    # List all files in the folder
+    input_files = os.listdir(input_output_folder)
 
     for filename in input_files:
-        input_path = os.path.join(input_folder, filename)
-        output_path = os.path.join(output_folder, filename)
+        input_path = os.path.join(input_output_folder, filename)
+        output_path = os.path.join(input_output_folder, filename)
 
         # Open the image
         with Image.open(input_path) as img:
@@ -25,11 +25,11 @@ def resize_images(input_folder, output_folder, target_size=(640, 640)):
     print(f'Finished resizing {len(input_files)} images.')
 
 # Example usage:
-#folder = 'datasets/data/train/images'
-#resize_images(folder, folder)
+folder = 'datasets/data/train/images'
+resize_images(folder, folder)
 folder = 'datasets/data/train/labels'
-resize_images(folder, folder)
+resize_images(folder)
 folder = 'datasets/data/test/images'
-resize_images(folder, folder)
+resize_images(folder)
 folder = 'datasets/data/test/labels'
-resize_images(folder, folder)
+resize_images(folder)
